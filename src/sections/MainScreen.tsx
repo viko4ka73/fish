461bec6082dd34f91bc2f7b0daeb2ddeac3dee36
dmitrieva vika <victoria.dmitrieva@spbpu.com>
2023-10-24 11:45:12 +0300

    mainScreen

--- src/sections/MainScreen.tsx
+++ src/sections/MainScreen.tsx
@@ -10,8 +10,8 @@ const MainScreen = () => {
                     alt='waves'
                     className="mobile-img-end w-[400px]"
                 />
-                <div className="flex flex-col items-center mb-6 max-[611px]:px-0.5 max-[890px]:px-1 max-[965px]:px-5  max-[410px]:mb-2">
-                    <h1 className="text-[64px] mobile-text-header font-montserrat font-bold leading-[123%]  text-white text-center mb-16 max-md:mb-3">
+                <div className="flex flex-col items-center mb-6  max-[611px]:px-0.5 max-[890px]:px-1 max-[965px]:px-5  max-[410px]:mb-2">
+                    <h1 className="text-[64px] mobile-text-header font-montserrat font-bold leading-[123%] max-[1800px]:mb-10 text-white text-center mb-16 max-md:mb-3 max-xl:mb-4 ">
                         <span className="text-dark-blue text-[74px] mobile-text-span "> Окунитесь </span>
                         в мир  морепродуктов с нашим  разнообразием и качеством!</h1>
                     <Button label="Оформить заказ" href="/order" Order={true} />
